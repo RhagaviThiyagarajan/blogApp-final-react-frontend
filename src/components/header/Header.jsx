72ef2a3e6f1ac7c0f28ad58158e0054437a2297e
Rhagavi Thiyagarajan <rhagavi3@gmail.com>
2022-11-03 20:11:40 +0530

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       </div>
       <img
         className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://cdn.dribbble.com/users/460129/screenshots/6195419/media/07adc74d75181b4dbfbd946c4dc7c7dc.png?compress=1&resize=1200x900&vertical=top"
         alt=""
       />
     </div>
